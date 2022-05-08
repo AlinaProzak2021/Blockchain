f79fdac7393d2bd58e9bc6a6e87a44c7aec3e09c
AlinaProzak2021 <foxic210@gmail.com>
2022-05-08 21:22:25 +0300

    create project

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Blockchain
-frontend for blockchain application. The concept is to track the results of each day
+# blockchain
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/nav-bar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="nav-bar">
+      <div class="nav-bar__items">
+          <div class="nav-bar__item">
+            Главная 
+          </div>
+        <div class="nav-bar__item">
+          О нас
+        </div>
+      </div>
+    <div class="nav-bar__auth">
+      <div class="nav-bar__sign-in">
+        Войти 
+      </div>
+      <div class="nav-bar__logo">
+        <img src="../../res/Logo.png" alt="">
+      </div>
+    </div>
+  </div>
+
+</template>
+<script>
+export default{
+}
+</script>
+<style>
+.nav-bar{
+  width: 100%;
+  height: 216px;
+  background: #FAFAFA;
+  display: flex;
+  justify-content: space-around;
+}
+.nav-bar__items{
+  width: 400px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.nav-bar__auth{
+  width: 300px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.nav-bar__logo{
+  width: 56px;
+  height: 56px;
+}
+
+
+</style>
